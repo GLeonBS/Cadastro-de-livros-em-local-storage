c5a8fa040c70b99872435cba67e872298256ea86
GLeonBS <gabriel.leonbs@gmail.com>
2023-05-10 22:23:33 -0300

    Refazendo projeto

--- public/entities/Document.js
+++
@@ -1,9 +0,0 @@
-export class Document {
-    constructor(title, subtitle, publishedAt, author) {
-        this.title = title;
-        this.subtitle = subtitle;
-        this.publishedAt = publishedAt;
-        this.author = author;
-    }
-}
-export default Document;
